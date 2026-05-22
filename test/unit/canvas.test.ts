342f07ff74f12e1670d41cd2448d4cf1805e43ef
intermittent.energy <git@intermittent.energy>
2026-05-22 18:58:51 +0200

    Increase echarts smoketest timeout

--- test/unit/canvas.test.ts
+++ test/unit/canvas.test.ts
@@ -42,6 +42,8 @@ async function renderEchartsCanvasPng() {
   }
 }
 
+const echartsSsrCanvasTimeoutMs = 30_000;
+
 describe("@napi-rs/canvas", () => {
   it("renders a PNG directly", async () => {
     const png = await renderCanvasPng();
@@ -55,5 +57,5 @@ describe("@napi-rs/canvas", () => {
 
     expect(png.subarray(0, 8)).toEqual(Buffer.from([0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a]));
     expect(png.length).toBeGreaterThan(1000);
-  });
+  }, echartsSsrCanvasTimeoutMs);
 });
