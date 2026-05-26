5e6e01732fc594e00824db14aa1bb76853c23fa8
intermittent.energy <git@intermittent.energy>
2026-05-26 21:06:08 +0200

    getLoadSeries() SUM per time

--- src/dashboards/shared/load.ts
+++ src/dashboards/shared/load.ts
@@ -3,15 +3,16 @@ import { chartQuery } from "./chartQuery.ts";
 import type { Series, TimeMetricValueRow } from "./types.ts";
 
 const loadSql = `
-  SELECT EXTRACT(EPOCH FROM time AT TIME ZONE $5) * 1000 AS time, metric, value
+  SELECT EXTRACT(EPOCH FROM time AT TIME ZONE $5) * 1000 AS time, 'load' AS metric, SUM(value) AS value
   FROM (
-    SELECT time_bucket_gapfill($1::interval, time) AS time, CONCAT(a.code, '/load') AS metric, INTERPOLATE(AVG(value)) AS value
+    SELECT time_bucket_gapfill($1::interval, time) AS time, INTERPOLATE(AVG(value)) AS value
     FROM load l
-    INNER JOIN areas a ON(l.area_id = a.id)
-    WHERE time BETWEEN $2 AND $3 AND area_id = ANY($4::int[])
-    GROUP BY 1, 2
-    ORDER BY 2, 1
+    WHERE time BETWEEN $2 AND $3 AND
+    area_id = ANY($4::int[])
+    GROUP BY 1, area_id
   ) s
+  GROUP BY 1
+  ORDER BY 1
 `;
 
 export async function getLoadSeries(
